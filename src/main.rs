6bfb511fdf22ea19334e78917bc54bef0701ee7f
Ifrahim Ansari <147778089+IfrahimA@users.noreply.github.com>
2025-05-18 00:11:17 -0400

    read from command-line and parse file contents

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let contents = fs::read_to_string(file_path).expect("Should be able to read this file.");
 }
